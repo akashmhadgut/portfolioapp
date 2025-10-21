e9d844b1b1ff113a02659a673fdc8bb5a3cec10e
akashmhadgut <akashmhadgut280@gmail.com>
2025-10-22 01:12:55 +0530

    Fix NavBar import case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from './assets/About.jsx';
 import Contact from './assets/Contact.jsx';
 import Footer from './assets/Footer.jsx';
 import Home from './assets/Home.jsx';
-import NavBar from './assets/Navbar.jsx';
+import NavBar from './assets/NavBar.jsx';
 import Projects from './assets/Projects.jsx';
 import Resume from './assets/Resume.jsx';
 
